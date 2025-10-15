a8131f10f3bb5868bacc6ea0343d6966e286a5a3
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-10-15 15:04:14 +0200

    commit

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -1,3 +1,11 @@
+// Fonction qui vérifie si un nombre est premier
+function estPremier(n) {
+  if (n < 2) return false; // Les nombres < 2 ne sont pas premiers
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false; // Si n est divisible par i, ce n'est pas un nombre premier
+  }
+  return true; // Sinon, c'est un nombre premier
+}
 // Fonction qui retourne la somme si les deux nombres sont premiers, sinon false
 function sommenombrespremiers(a, b) {
   if (estPremier(a) && estPremier(b)) {
