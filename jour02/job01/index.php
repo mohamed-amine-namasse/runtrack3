10bde7704fbda48104c56d39dec7bfadaf5dfd2f
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-10-16 11:17:00 +0200

    commit

---
+++ jour02/job01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="./script.js"></script>
+
+</head>
+
+<body>
+
+  <article id="citation">
+    La vie a beaucoup plus d’imagination que nous
+  </article>
+
+
+  <button id="button" onclick="citation()">Afficher la citation</button>
+
+</body>
+
+</html>
